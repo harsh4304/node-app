77cf6628d9e467130f06001a2855d34c3654417a
harsh4304 <harsh43431101@gmail.com>
2024-05-06 18:11:10 +0530

    added connection details in env and used it in config file

--- Backend/db/config/config.js
+++ Backend/db/config/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
     "development": {
-      "username": "root",
-      "password": "$Harsh12345",
-      "database": "new_task_2",
-      "host": "localhost",
-      "dialect": "mysql"
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_NAME,
+      "host": process.env.DB_HOST,
+      "dialect": process.env.DIALECT
     },
     "test": {
       "username": "root",
